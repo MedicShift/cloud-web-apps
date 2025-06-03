4d8e697abb0718d63ada2a09e1cfa20a39097da5
Alwin Sam Roy <alwinsamroy2002@gmail.com>
2025-06-03 17:31:09 +0530

    db migrations

---
+++ CoreApiApp/WebApp/Migrations/20250603115657_Initial.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoreApiApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DataProtectionKeys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FriendlyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Xml = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataProtectionKeys", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hospital",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    Name = table.Column<string>(type: "NVARCHAR(256)", nullable: false),
+                    EmailId = table.Column<string>(type: "NVARCHAR(256)", nullable: false),
+                    Address = table.Column<string>(type: "varchar(512)", nullable: false),
+                    ContactInfo = table.Column<string>(type: "varchar(256)", nullable: false),
+                    SetupStatus = table.Column<byte>(type: "tinyint", nullable: false, defaultValue: (byte)0),
+                    PlanStatus = table.Column<byte>(type: "tinyint", nullable: false, defaultValue: (byte)0),
+                    PlanExpiresOn = table.Column<DateTime>(type: "datetime", nullable: false, defaultValueSql: "DATEADD(day, 30, GETUTCDATE())"),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hospital", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    Name = table.Column<string>(type: "NVARCHAR(256)", nullable: false),
+                    HospitalId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Department_Hospital_HospitalId",
+                        column: x => x.HospitalId,
+                        principalTable: "Hospital",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Designation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    Title = table.Column<string>(type: "NVARCHAR(256)", nullable: false),
+                    HospitalId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Designation", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Designation_Hospital_HospitalId",
+                        column: x => x.HospitalId,
+                        principalTable: "Hospital",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shift",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Guid = table.Column<Guid>(type: "UNIQUEIDENTIFIER", nullable: false, defaultValueSql: "NEWID()"),
+                    ShiftType = table.Column<string>(type: "NVARCHAR(256)", nullable: false),
+                    StartTime = table.Column<string>(type: "NVARCHAR(256)", nullable: false),
+                    EndTime = table.Column<string>(type: "NVARCHAR(256)", nullable: false),
+                    HospitalId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "DATETIME2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "DATETIME2", nullable: false, defaultValueSql: "GETUTCDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shift", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Shift_Hospital_HospitalId",
+                        column: x => x.HospitalId,
+                        principalTable: "Hospital",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Staff",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    FirstName = table.Column<string>(type: "NVARCHAR(256)", nullable: false),
+                    LastName = table.Column<string>(type: "NVARCHAR(256)", nullable: false),
+                    EmailId = table.Column<string>(type: "NVARCHAR(256)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "NVARCHAR(max)", nullable: false),
+                    HospitalId = table.Column<int>(type: "int", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: true),
+                    DesignationId = table.Column<int>(type: "int", nullable: false),
+                    IsAdmin = table.Column<bool>(type: "BIT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Staff", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Staff_Department_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Department",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Staff_Designation_DesignationId",
+                        column: x => x.DesignationId,
+                        principalTable: "Designation",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Staff_Hospital_HospitalId",
+                        column: x => x.HospitalId,
+                        principalTable: "Hospital",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Schedule",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    StaffId = table.Column<int>(type: "int", nullable: false),
+                    ShiftId = table.Column<int>(type: "int", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false),
+                    ScheduledDate = table.Column<string>(type: "NVARCHAR(255)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedule", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Schedule_Department_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Department",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Schedule_Shift_ShiftId",
+                        column: x => x.ShiftId,
+                        principalTable: "Shift",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Schedule_Staff_StaffId",
+                        column: x => x.StaffId,
+                        principalTable: "Staff",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Department_HospitalId",
+                table: "Department",
+                column: "HospitalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Designation_HospitalId",
+                table: "Designation",
+                column: "HospitalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedule_DepartmentId",
+                table: "Schedule",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedule_ShiftId",
+                table: "Schedule",
+                column: "ShiftId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedule_StaffId",
+                table: "Schedule",
+                column: "StaffId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shift_HospitalId",
+                table: "Shift",
+                column: "HospitalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Staff_DepartmentId",
+                table: "Staff",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Staff_DesignationId",
+                table: "Staff",
+                column: "DesignationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Staff_HospitalId",
+                table: "Staff",
+                column: "HospitalId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DataProtectionKeys");
+
+            migrationBuilder.DropTable(
+                name: "Schedule");
+
+            migrationBuilder.DropTable(
+                name: "Shift");
+
+            migrationBuilder.DropTable(
+                name: "Staff");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+
+            migrationBuilder.DropTable(
+                name: "Designation");
+
+            migrationBuilder.DropTable(
+                name: "Hospital");
+        }
+    }
+}
