181e75e5b56b212dae2153ed77c02430780e55e1
Alwin Sam Roy <alwinsamroy2002@gmail.com>
2025-06-04 21:02:56 +0530

    migration chg

---
+++ CoreApiApp/WebApp/Migrations/20250604152941_RemovedHospitalIdColum.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoreApiApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedHospitalIdColum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Designation_Hospital_HospitalId",
+                table: "Designation");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Designation_HospitalId",
+                table: "Designation");
+
+            migrationBuilder.DropColumn(
+                name: "HospitalId",
+                table: "Designation");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "HospitalId",
+                table: "Designation",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Designation_HospitalId",
+                table: "Designation",
+                column: "HospitalId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Designation_Hospital_HospitalId",
+                table: "Designation",
+                column: "HospitalId",
+                principalTable: "Hospital",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
