257ed36a871e2066c6ec6dd5d734f9b894df89e0
Alwin Sam Roy <alwinsamroy2002@gmail.com>
2025-06-06 15:27:08 +0530

    added request table

--- CoreApiApp/WebApp/Data/Entities/Request.cs
+++ CoreApiApp/WebApp/Data/Entities/Request.cs
@@ -4,10 +4,11 @@ public class Request
 {
     public int Id { get; set; }
     public Guid Guid { get; set; }
-    public int StaffId { get; set; }
-    public int Status { get; set; }
+    public RequestType RequestType { get; set; }
     public int TypeId { get; set; }
+    public Staff Staff { get; set; }
+    public int RequestedBy { get; set; }
+    public int Status { get; set; }
     public DateTime RequestedAt { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime? DeletedAt { get; set; }
+    public LeaveRequest LeaveRequest { get; set; }
 }
---
+++ CoreApiApp/WebApp/Migrations/20250606095245_AddRequestTables.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoreApiApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRequestTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RequestType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    Name = table.Column<string>(type: "NVARCHAR(256)", nullable: false),
+                    Description = table.Column<string>(type: "NVARCHAR(512)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RequestType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Request",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    TypeId = table.Column<int>(type: "int", nullable: false),
+                    RequestedBy = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<byte>(type: "TINYINT", nullable: false, defaultValue: (byte)0),
+                    RequestedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Request", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Request_RequestType_TypeId",
+                        column: x => x.TypeId,
+                        principalTable: "RequestType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Request_Staff_RequestedBy",
+                        column: x => x.RequestedBy,
+                        principalTable: "Staff",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LeaveRequest",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RequestId = table.Column<int>(type: "int", nullable: false),
+                    StartDate = table.Column<string>(type: "NVARCHAR(256)", nullable: false),
+                    EndDate = table.Column<string>(type: "NVARCHAR(256)", nullable: false),
+                    Reason = table.Column<string>(type: "NVARCHAR(256)", nullable: false),
+                    ReviewedBy = table.Column<int>(type: "int", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaveRequest", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LeaveRequest_Request_RequestId",
+                        column: x => x.RequestId,
+                        principalTable: "Request",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LeaveRequest_Staff_ReviewedBy",
+                        column: x => x.ReviewedBy,
+                        principalTable: "Staff",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveRequest_RequestId",
+                table: "LeaveRequest",
+                column: "RequestId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveRequest_ReviewedBy",
+                table: "LeaveRequest",
+                column: "ReviewedBy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Request_RequestedBy",
+                table: "Request",
+                column: "RequestedBy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Request_TypeId",
+                table: "Request",
+                column: "TypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LeaveRequest");
+
+            migrationBuilder.DropTable(
+                name: "Request");
+
+            migrationBuilder.DropTable(
+                name: "RequestType");
+        }
+    }
+}
