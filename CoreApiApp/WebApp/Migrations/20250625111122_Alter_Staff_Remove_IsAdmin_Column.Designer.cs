bb64c33d3ee8de649d0b5bf26c692373ce667bef
Alwin Sam Roy <alwinsamroy2002@gmail.com>
2025-06-25 16:45:35 +0530

    remove isAdmin column

--- CoreApiApp/WebApp/Migrations/20250625111122_Alter_Staff_Remove_IsAdmin_Column.Designer.cs
+++ CoreApiApp/WebApp/Migrations/20250625111122_Alter_Staff_Remove_IsAdmin_Column.Designer.cs
@@ -4,6 +4,7 @@ using CoreApiApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CoreApiApp.Migrations
 {
     [DbContext(typeof(CoreDbContext))]
-    partial class CoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250625111122_Alter_Staff_Remove_IsAdmin_Column")]
+    partial class Alter_Staff_Remove_IsAdmin_Column
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
