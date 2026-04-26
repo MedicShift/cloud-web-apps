1247fa033e5180fc6409839752b6e12a4add4e90
alwin <alwinsamroy2002@gmail.com>
2026-04-26 20:46:51 +0200

    chgs

--- medicshift/src/common/entities/base.entity.ts
+++ medicshift/src/common/entities/base.entity.ts
@@ -2,10 +2,6 @@ import { PrimaryGeneratedColumn, BaseEntity as TypeOrmBaseEntity, Column, Create
 
 export abstract class BaseEntity extends TypeOrmBaseEntity {
   
-  @ApiProperty({
-    example: '123e4567-e89b-12d3-a456-426614174000',
-    description: 'The unique UUID of the entity',
-  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
